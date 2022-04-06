9ae71c171ec9938e6e537ec7e891df9ed452f71d
Krina Darji <96282036+KrinaDarji@users.noreply.github.com>
2022-04-06 11:58:49 +0530

    Add files via upload

---
+++ Practical_15/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practical_15.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            this.logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogInformation("Index method Called");
+            logger.LogWarning("Index method Called Warning");
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
